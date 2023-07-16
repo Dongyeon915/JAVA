5846119a424c2afa6a95de58dba785dd447d3362
DongYeonKim <109599149+Dongyeon915@users.noreply.github.com>
2023-07-16 11:25:09 +0900

    Update ConsumerExample.java

--- src/functionPackage/ConsumerExample.java
+++ src/functionPackage/ConsumerExample.java
@@ -9,7 +9,7 @@ public class ConsumerExample  {
 
     public static void main(String[] args) {
 
-//     배개변수만 있고 반환값이 없다
+//     매개변수만 있고 반환값이 없다
         Consumer<Integer> c = i -> System.out.println(i);
         c.accept(3);
 
